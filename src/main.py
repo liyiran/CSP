87fd82e28eb25baf53abebb180254e608b41decd
liyiran <liy@worldticket.net>
2018-02-26 19:14:32 -0800

    min conflict resolver

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Configuration:
     def __init__(self, conf):
         configs = conf.splitlines()
@@ -14,3 +17,100 @@ class Configuration:
             one_team = team_string.split(',')
             if not one_team[0] == 'None':
                 self.teams[team_name] = one_team
+
+
+class MinConflictSolver:
+    def __init__(self):
+        self.constraints = []
+        self.domains = {}
+        self.vconstraints = {}
+
+    def add_variable(self, variables, domain):
+        """
+        :param variables: list like [a,b,c] 
+        :param domain: list like [1,2,3]
+        :return: 
+        """
+        for var in variables:
+            self.domains[var] = domain
+
+    def add_constraint(self, constraint, variables):
+        """
+        :param constraint: constraint like AllDifferent 
+        :param variables: list like [a,b,c]
+        :return: 
+        """
+        # for var in variables:
+        self.constraints.append((constraint, variables))
+
+    def get_solution(self, max_step=1000):
+        # recording the constraint and all related variables for this variable
+        for variables in self.domains:
+            self.vconstraints[variables] = []
+        for constraint, variables in self.constraints:
+            for variable in variables:
+                self.vconstraints[variable].append((constraint, variables))
+        assignment = {}
+        for var in self.domains:
+            # random initialize variable
+            assignment[var] = random.choice(self.domains[var])
+        for _ in range(max_step):
+            conflicted = False
+            var_list = self.domains.keys()
+            for var in var_list:
+                # find conflict
+                for constraint, variables in self.vconstraints[var]:
+                    # find all related constraints and all variables that related to this constraint
+                    if not constraint(variables, self.domains, assignment):
+                        # a conflict for this variable assignment, resolve it
+                        break
+                else:
+                    continue  # move to the next variable
+                # resolve conflict
+                min_count = len(self.vconstraints[var])  # at most len(self.vconstraints[var]) conflicts
+                values = []  # least conflict assignment condidates
+                for value in self.domains[var]:
+                    # all possible assignments
+                    assignment[var] = value  # assignment the value 
+                    counter = 0
+                    for constraint, variables in self.vconstraints[var]:
+                        if not constraint(variables, self.domains, assignment):
+                            counter += 1  # one conflict
+                    if counter == min_count:
+                        values.append(value)
+                    elif counter < min_count:
+                        # less conflicts
+                        min_count = counter
+                        values = [value]
+                assignment[var] = random.choice(values)  # randomly choose a value
+                conflicted = True
+            if not conflicted:
+                return assignment
+        return None
+
+
+class AllDifferentConstraint(object):
+    def __call__(self, variables, domains, assignments):
+        found = []
+        for var in variables:
+            value = assignments.get(var)
+            if value is not None:
+                if value in found:
+                    return False
+                found.append(value)
+        return True
+
+
+class AtMostTwoConstraint:
+    def __call__(self, variables, domains, assignments):
+        map = {}
+        for var in variables:
+            value = assignments.get(var, None)
+            if value is not None:
+                if not value in map:
+                    map[value] = 1
+                elif map[value] == 1:
+                    map[value] = 2
+                else:
+                    return False
+        return True
--- src/test_configuration.py
+++ src/test_configuration.py
@@ -1,6 +1,7 @@
+from collections import defaultdict
 from unittest import TestCase
 
-from main import Configuration
+from main import AllDifferentConstraint, AtMostTwoConstraint, Configuration, MinConflictSolver
 
 
 class TestConfiguration(TestCase):
@@ -28,3 +29,95 @@ UEFA:France,Germany,Italy,England,Russia''')
         self.assertListEqual(list(configuration.teams.values())[1], ['USA', 'Mexico'])
         self.assertListEqual(list(configuration.teams.values())[0], ['Brazil', 'Argentina'])
         self.assertListEqual(list(configuration.teams.values())[2], ['France', 'Germany', 'Italy', 'England', 'Russia'])
+
+    def test_small(self):
+        configuration = Configuration(
+            '''3
+4
+Russia,Brazil,Argentina
+Germany,Italy,England
+France,Iran,Mexico
+USA,Japan,Australia
+AFC:Iran,Japan,Australia
+CAF:None
+OFC:None
+CONCACAF:USA,Mexico
+CONMEBOL:Brazil,Argentina
+UEFA:France,Germany,Italy,England,Russia''')
+        minConflictSolver = MinConflictSolver()
+        for pot in configuration.pots.values():
+            minConflictSolver.add_variable(pot, range(0, configuration.group))
+            minConflictSolver.add_constraint(AllDifferentConstraint(), pot)
+        for team_name, teams in configuration.teams.iteritems():
+            if team_name == 'UEFA':
+                minConflictSolver.add_constraint(AtMostTwoConstraint(), teams)
+            else:
+                minConflictSolver.add_constraint(AllDifferentConstraint(), teams)
+        solution = minConflictSolver.get_solution()
+        output = defaultdict(list)
+        if solution is not None:
+            for team_name, group in solution.iteritems():
+                output[group].append(team_name)
+        print(output)
+
+    def test_big_no_result(self):
+        configuration = Configuration(
+            '''8
+5
+Russia,Germany,Brazil,Portugal,Argentina,Belgium,Poland,France
+Spain,Peru,Switzerland,England,Colombia,Mexico,Uruguay,Croatia
+Denmark,Iceland,Costa Rica,Sweden,Tunisia,Egypt,Senegal,Iran
+Australia,Japan,Morocco,Panama,South Korea,Saudi Arabia,USA,Bolivia
+Chile,Paraguay,Serbia,Nigeria,Italy,Netherlands,Czech Republic,China
+AFC:South Korea,Saudi Arabia,Iran,Japan,Australia,China
+CAF:Tunisia,Egypt,Senegal,Morocco,Nigeria
+CONCACAF:Mexico,Panama,Costa Rica,USA
+CONMEBOL:Brazil,Argentina,Uruguay,Colombia,Peru,Bolivia,Chile,Paraguay
+UEFA:France,Germany,England,Russia,Portugal,Belgium,Poland,Switzerland,Croatia,Denmark,Iceland,Serbia,Spain,Sweden,Italy,Netherlands,Czech Republic
+OFC:None''')
+        minConflictSolver = MinConflictSolver()
+        for pot in configuration.pots.values():
+            minConflictSolver.add_variable(pot, range(0, configuration.group))
+            minConflictSolver.add_constraint(AllDifferentConstraint(), pot)
+        for team_name, teams in configuration.teams.iteritems():
+            if team_name == 'UEFA':
+                minConflictSolver.add_constraint(AtMostTwoConstraint(), teams)
+            else:
+                minConflictSolver.add_constraint(AllDifferentConstraint(), teams)
+        solution = minConflictSolver.get_solution()
+        output = defaultdict(list)
+        if solution is not None:
+            for team_name, group in solution.iteritems():
+                output[group].append(team_name)
+        self.assertEqual(0, len(output))
+
+    def test_big_has_result(self):
+        configuration = Configuration(
+            '''8
+5
+Russia,Germany,Brazil,Portugal,Argentina,Belgium,Poland,France
+Spain,Peru,Switzerland,England,Colombia,Mexico,Uruguay
+Denmark,Iceland,Costa Rica,Sweden,Tunisia,Egypt,Senegal,Iran
+Australia,Japan,Morocco,Panama,South Korea,Saudi Arabia,USA
+Chile,Paraguay,Serbia,Nigeria,Italy,Netherlands,Czech Republic
+AFC:South Korea,Saudi Arabia,Japan,Australia,Iran
+CAF:Tunisia,Egypt,Senegal,Morocco,Nigeria
+CONCACAF:Mexico,Panama,Costa Rica,USA
+CONMEBOL:Brazil,Argentina,Uruguay,Colombia,Peru,Chile,Paraguay
+UEFA:France,Germany,England,Russia,Portugal,Belgium,Poland,Switzerland,Denmark,Iceland,Serbia,Spain,Sweden,Italy,Netherlands,Czech Republic
+OFC:None''')
+        minConflictSolver = MinConflictSolver()
+        for pot in configuration.pots.values():
+            minConflictSolver.add_variable(pot, range(0, configuration.group))
+            minConflictSolver.add_constraint(AllDifferentConstraint(), pot)
+        for team_name, teams in configuration.teams.iteritems():
+            if team_name == 'UEFA':
+                minConflictSolver.add_constraint(AtMostTwoConstraint(), teams)
+            else:
+                minConflictSolver.add_constraint(AllDifferentConstraint(), teams)
+        solution = minConflictSolver.get_solution()
+        output = defaultdict(list)
+        if solution is not None:
+            for team_name, group in solution.iteritems():
+                output[group].append(team_name)
+        print(output)
